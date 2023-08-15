a55c52044441c63aeb584dfd3155e500d4061f5c
MuursaleenBats <mursaleenbatatawala@gmail.com>
2023-08-15 16:25:09 +0530

    Used consumer widget for refactoring the code.

--- lib/models/tasks_data.dart
+++ lib/models/tasks_data.dart
@@ -3,4 +3,8 @@ import 'package:todoey_flutter/models/task.dart';
 
 class TaskData extends ChangeNotifier {
   List<Task> tasks = [Task(name: 'Buy milk')];
+
+  int get taskCount {
+    return tasks.length;
+  }
 }
--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -6,19 +6,23 @@ import 'package:provider/provider.dart';
 class TasksList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemBuilder: (context, index) {
-          return TaskTile(
-            isChecked: Provider.of<TaskData>(context).tasks[index].isDone,
-            title: Provider.of<TaskData>(context).tasks[index].name,
-            toggleCheckboxState: (bool checkboxState) {
-              // setState(() {
-              //   Provider.of<TaskData>(context).tasks[index].isDone =
-              //       checkboxState;
-              // });
+    return Consumer<TaskData>(
+      builder: (context, taskData, child) {
+        return ListView.builder(
+            itemBuilder: (context, index) {
+              return TaskTile(
+                isChecked: taskData.tasks[index].isDone,
+                title: taskData.tasks[index].name,
+                toggleCheckboxState: (bool checkboxState) {
+                  // setState(() {
+                  //   Provider.of<TaskData>(context).tasks[index].isDone =
+                  //       checkboxState;
+                  // });
+                },
+              );
             },
-          );
-        },
-        itemCount: Provider.of<TaskData>(context).tasks.length);
+            itemCount: taskData.taskCount);
+      },
+    );
   }
 }
